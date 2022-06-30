68b6137de89d1356339180986296002e5de545b6
Tomislav <tom.mikulcic@gmail.com>
2022-06-30 13:28:48 +0200

    Data and images stored for the page

---
+++ src/components/data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    title: 'Pula',
+    location: 'Croatia',
+    googleMapsUrl: 'https://goo.gl/maps/scx3W3KFaPxGzSHs6',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'Pula, grad u Hrvatskoj. Najveći je grad Istarske županije, leži na jugozapadnom području istarskog poluotoka u dobro zaštićenom zaljevu. Grad ima dugu tradiciju vinarstva, ribarstva, brodogradnje i turizma, a ujedno je i tranzitna luka. Pula je kulturni centar Istre još od rimskoga doba.',
+  },
+  {
+    title: 'Murter',
+    location: 'Croatia',
+    googleMapsUrl: 'https://goo.gl/maps/a9NVrknL1mzeME8TA',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'Murter je na sjeverozapadnom dijelu istoimenog otoka udaljeno 32 kilometra od Šibenika i 65 kilometara od Zadra, okružen brežuljcima Radučem (125 m), Sv. Rokom (70 m), i Gradinom (62 m). Murter se širi prema uvali Hramini, u kojoj je izgrađena marina, prema uvali Slanica. Osim plaže Slanice, Murter ima još četiri plaže, a to su Luke, Podvrške, Čigrađa i Kosirina.',
+  },
+  {
+    title: 'Split',
+    location: 'Croatia',
+    googleMapsUrl: 'https://goo.gl/maps/fYGT7iuXSrugTHQG6',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'Split je najveći grad u Dalmaciji, te po broju stanovnika i drugi najveći grad u Hrvatskoj, a predstavlja gospodarsko i kulturno središte Dalmacije.',
+  },
+  {
+    title: 'Dubrovnik',
+    location: 'Croatia',
+    googleMapsUrl: 'https://goo.gl/maps/UdH5mrKBWmvCWmLt5',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'Dubrovnik je grad na krajnjem jugu Hrvatske. Administrativno je središte Dubrovačko-neretvanske županije i jedno od najvažnijih povijesno-turističkih središta Hrvatske.',
+  },
+];
